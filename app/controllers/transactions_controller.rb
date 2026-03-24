596d88bdf01672c064242f0dbbc4bd58dadbbdb6
Hiroto Arakawa <a.hiroto0914@gmail.com>
2026-03-24 14:16:13 +0900

    feat: 収支CRUDを実装

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,51 @@
+class TransactionsController < ApplicationController
+    before_action :set_transaction, only: %i[show edit update destroy]
+
+    def index
+        @transactions = Transaction.recent
+    end
+
+    def show
+    end
+
+    def new
+        @transaction = Transaction.new
+    end
+
+    def create
+        @transaction = Transaction.new(transaction_params)
+        if @transaction.save
+            redirect_to transactions_path, notice: "収支を登録しました"
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @transaction.update(transaction_params)
+            redirect_to transactions_path, notice: "収支を登録しました"
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @transaction.destroy
+        redirect_to transactions_path, notice: "収支を削除しました"
+    end
+
+    private
+
+    def set_transaction
+        @transaction = Transaction.find(params[:id])
+    end
+
+    def transaction_params
+        params.require(:transaction).permit(
+            :transaction_type, :amount, :date, :memo
+        )
+    end
+end
